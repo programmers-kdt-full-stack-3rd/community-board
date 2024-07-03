cc7d6f697f7d422c2e40436749e5fec119bd21e1
Siltarae <sjkwang1@gmail.com>
2024-07-03 19:24:10 +0900

    feat: created_at 제거,  expires_at -> expired_at로 변경

--- server/db/sql/refresh_tokens.sql
+++ server/db/sql/refresh_tokens.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     token TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    expires_at TIMESTAMP NOT NULL,
+    expired_at TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
