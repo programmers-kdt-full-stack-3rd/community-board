ad37f867a4d95c1cd1f7f466f5d317e3e681034c
Siltarae <sjkwang1@gmail.com>
2024-07-03 16:22:00 +0900

    feat: refresh_token 테이블 추가

---
+++ server/db/sql/refresh_tokens.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    token TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
