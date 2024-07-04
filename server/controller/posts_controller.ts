610e0d96308ab4fb0ba2c5d0678f03a034122095
HungKungE <jonsun59@gmail.com>
2024-07-05 01:22:27 +0900

    feat: posts controller 추가 + posts 목록 불러오는 API 기능 구현

---
+++ server/controller/posts_controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { getPostInfos } from '../db/context/posts_context';
+
+export interface PostRequest {
+    index : number;
+    perPage : number;
+    keyword : string | null;
+}
+
+export const getPosts = async (req : Request, res : Response, next : NextFunction) => {
+    try{
+        const values : PostRequest = {
+            index : parseInt(req.query.index as string) - 1 || 0,
+            perPage : parseInt(req.query.perPage as string) || 10,
+            keyword : req.query.keyword as string || null
+        };
+        const posts = await getPostInfos(values);
+        res.json({ posts });
+    }catch(err){
+        next(err);
+    }
+}
