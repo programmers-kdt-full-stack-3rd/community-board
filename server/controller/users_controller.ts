5a824acae02fd0a95650f86f98768c5bca55eed2
Siltarae <sjkwang1@gmail.com>
2024-07-06 13:42:55 +0900

    feat: user_controller에 회원가입 핸들러 구현

---
+++ server/controller/users_controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { addUser } from "../db/context/users_context";
+import { ServerError } from "../middleware/errors";
+
+// 비동기로 salt를 생성하는 함수
+
+export const postJoin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const values = {
+      email: req.body.email,
+      password: req.body.password,
+      nickname: req.body.nickname,
+    };
+
+    const result = await addUser(values);
+    if (result.affectedRows === 1) {
+      res.status(201).json({ message: "회원가입 성공" });
+    } else {
+      next(ServerError.reference("회원가입 실패"));
+    }
+  } catch (err: any) {
+    if (err.code === "ER_DUP_ENTRY") {
+      next(ServerError.badRequest("이미 존재하는 이메일 주소입니다."));
+    }
+  }
+};
