f3a986193ecc4fea781a7b484e4c27f660abd1b7
HungKungE <jonsun59@gmail.com>
2024-07-08 19:45:18 +0900

    feat : 게시글 생성 api route 추가 + postBody 검증 arr 추가

--- server/route/posts_router.ts
+++ server/route/posts_router.ts
@@ -1,10 +1,27 @@
 import express from 'express';
-import { getPost, getPosts } from '../controller/posts_controller';
+import { createPost, getPost, getPosts } from '../controller/posts_controller';
+import { body } from "express-validator";
+import { validate } from "../middleware/validate";
+
+// Request body 유효성 검사
+const postBodyValidation = [
+  body("title")
+    .notEmpty()
+    .withMessage("게시글 제목을 입력해주세요.")
+    .bail(),
+  body("content")
+    .notEmpty()
+    .withMessage("게시글 내용을 입력해주세요.")
+    .bail(),
+  validate,
+];
 
 const router = express.Router();
 router.use(express.json());
 
 router.get("/", getPosts);
 router.get("/:post_id", getPost);
+
+router.post("/", postBodyValidation, createPost);
 
 export default router;
