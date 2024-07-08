94dc1a42ecf101f3c96be3627d54e5a8ac2d589a
Siltarae <sjkwang1@gmail.com>
2024-07-08 22:55:21 +0900

    feat: 로그인 인증 기능 추가

--- server/db/context/users_context.ts
+++ server/db/context/users_context.ts
@@ -1,13 +1,28 @@
-import { FieldPacket, PoolConnection, ResultSetHeader } from "mysql2/promise";
+import {
+  FieldPacket,
+  PoolConnection,
+  ResultSetHeader,
+  RowDataPacket,
+} from "mysql2/promise";
 import pool from "../connect";
 import { makeHashedPassword, makeSalt } from "../../utils/crypto";
 import { ServerError } from "../../middleware/errors";
+import { IUser } from "../model/users";
+import { makeAccessToken, makeRefreshToken } from "../../utils/token";
+import { addToken } from "./token_context";
 
 interface IUserRegData {
   email: string;
   password: string;
   nickname: string;
 }
+
+interface IUserAuthData {
+  email: string;
+  password: string;
+}
+
+interface IUserAuthResult extends RowDataPacket, IUser {}
 
 export const addUser = async (userData: IUserRegData) => {
   let conn: PoolConnection | null = null;
@@ -34,6 +49,52 @@ export const addUser = async (userData: IUserRegData) => {
     } else {
       throw err;
     }
+  } finally {
+    if (conn) conn.release();
+  }
+};
+
+export const authUser = async (userData: IUserAuthData) => {
+  let conn: PoolConnection | null = null;
+  try {
+    const sql = `SELECT * FROM users WHERE email = ? AND isDelete=FALSE`;
+    const value = [userData.email];
+
+    let accessToken: string;
+    let refreshToken: string;
+
+    conn = await pool.getConnection();
+    const [rows]: [IUserAuthResult[], FieldPacket[]] = await conn.query(
+      sql,
+      value
+    );
+
+    if (rows.length === 0) {
+      throw ServerError.badRequest("존재하지 않는 이메일입니다.");
+    }
+
+    const user: IUserAuthResult = rows[0];
+
+    const hashedPassword: string = await makeHashedPassword(
+      userData.password,
+      user.salt
+    );
+
+    if (user.password !== hashedPassword) {
+      throw ServerError.badRequest("비밀번호가 일치하지 않습니다.");
+    } else {
+      accessToken = await makeAccessToken(user.id);
+      refreshToken = await makeRefreshToken(user.id);
+      addToken(
+        user.id,
+        refreshToken,
+        new Date(Date.now() + 1000 * 60 * 60 * 24)
+      );
+    }
+
+    return { accessToken, refreshToken };
+  } catch (err: any) {
+    throw err;
   } finally {
     if (conn) conn.release();
   }
