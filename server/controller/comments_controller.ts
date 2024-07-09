a8a12034ace0f990534b38fa7d3bbe67c50e0428
yejunian <leeye51456@gmail.com>
2024-07-09 17:01:46 +0900

    feat: controller에 댓글 생성 API 핸들러 구현

--- server/controller/comments_controller.ts
+++ server/controller/comments_controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import { getComments } from "../db/context/comment_context";
+import { createComment, getComments } from "../db/context/comment_context";
 import { ServerError } from "../middleware/errors";
 
-export const getCommentsByPostId = async (req: Request, res: Response, next: NextFunction) => {
+export const getCommentsByPostId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const postId = parseInt(req.params.post_id, 10);
     if (isNaN(postId)) {
@@ -11,6 +15,29 @@ export const getCommentsByPostId = async (req: Request, res: Response, next: Nex
 
     const comments = await getComments(parseInt(req.params.post_id));
     res.status(200).json({ comments });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const createCommentByPostId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const postId = parseInt(req.params.post_id, 10);
+    if (isNaN(postId)) {
+      throw ServerError.badRequest("Invalid post ID");
+    }
+
+    await createComment({
+      post_id: parseInt(req.params.post_id),
+      author_id: req.body.author_id,
+      content: req.body.content,
+    });
+
+    res.status(201).end();
   } catch (err) {
     next(err);
   }
