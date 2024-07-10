9ac6c7562b19174896fc75c329129ce68185d9ae
yejunian <leeye51456@gmail.com>
2024-07-10 23:13:29 +0900

    feat: 댓글 수정 API route 등록 및 유효성 검사 추가

--- server/route/comments_router.ts
+++ server/route/comments_router.ts
@@ -3,6 +3,7 @@ import { body, param } from "express-validator";
 import {
   handleCommentCreate,
   handleCommentsRead,
+  handleCommentUpdate,
 } from "../controller/comments_controller";
 import { validate } from "../middleware/validate";
 
@@ -32,10 +33,27 @@ const postCommentValidation = [
   validate,
 ];
 
+const patchCommentValidation = [
+  body("id")
+    .notEmpty()
+    .withMessage("댓글 ID를 입력해 주십시오.")
+    .bail()
+    .isInt({ min: 1 })
+    .withMessage("댓글 ID가 양의 정수가 아닙니다."),
+  body("content")
+    .notEmpty()
+    .withMessage("본문을 입력해 주십시오.")
+    .bail()
+    .isString()
+    .withMessage("본문이 문자열이 아닙니다."),
+  validate,
+];
+
 const router = express.Router();
 router.use(express.json());
 
 router.get("/:post_id", getCommentValidation, handleCommentsRead);
 router.post("/", postCommentValidation, handleCommentCreate);
+router.patch("/", patchCommentValidation, handleCommentUpdate);
 
 export default router;
