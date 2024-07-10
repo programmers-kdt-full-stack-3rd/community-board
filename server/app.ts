4e93e4f18c9861c4b102e199b9842c4698ee2050
Siltarae <sjkwang1@gmail.com>
2024-07-10 14:59:29 +0900

    feat: 쿠키를 가져오기위한 cookie-parser 설치 및 미들웨어 등록

--- server/app.ts
+++ server/app.ts
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 import apiRouter from "./route/route";
 // handelr ----------------
 import { errorHandler } from "./middleware/errors";
+// cookie-parser -------------
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
 // app 등록
+app.use(cookieParser());
 app.use("/api", apiRouter);
 app.use(errorHandler);
 
