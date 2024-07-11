d135a80c4717ca5ce9592de16b662443c0c562e4
HungKungE <jonsun59@gmail.com>
2024-07-11 22:33:15 +0900

    test : posts page에 예제 세팅 추가

--- client/src/component/Posts/Posts.tsx
+++ client/src/component/Posts/Posts.tsx
@@ -1,6 +1,17 @@
+import { sendGetTestRequest } from "../../api/test/test";
+
 const Posts = () => {
+
+  const getTest = () => {
+    sendGetTestRequest().then((res)=>{
+      console.log(res);
+    });
+  };
+
   return (
-    <div>Posts</div>
+    <div>
+      <button onClick={getTest}>테스트 보내기</button>
+    </div>
   )
 }
 
