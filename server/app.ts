f18971bffba0ad7f11156e0afec8db97c1dbf2bd
HungKungE <jonsun59@gmail.com>
2024-07-11 20:10:29 +0900

    feat : server 작동 후 접속 -> client 화면 제공

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
 // router -----------------
 import apiRouter from "./route/route";
 // handelr ----------------
@@ -7,10 +9,18 @@ import { errorHandler } from "./middleware/errors";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 
 // app 등록
 app.use("/api", apiRouter);
 app.use(errorHandler);
+
+// client 파일 제공
+app.use(express.static(`${__dirname}/../client/dist`));
+app.get(`*`, (req: Request, res: Response) => {
+  let indexPath = path.join(__dirname, "../client/dist/index.html");
+  res.sendFile(indexPath);
+});
 
 // listen
 app.listen(process.env.PORT, () => {
