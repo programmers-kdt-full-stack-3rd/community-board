19c51fa73889794a4acdb8f4aae15d899ea48dc9
yejunian <leeye51456@gmail.com>
2024-07-13 03:52:19 +0900

    feat: likes router 추가 및 좋아요 API 연결

---
+++ server/route/likes_router.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { requireLogin } from "../middleware/auth";
+import {
+  handleLikeCreateWith,
+  handleLikeDeleteWith,
+  handleLikesReadWith,
+} from "../controller/likes_controller";
+import { likeValidationWith } from "../utils/validations/likes/like";
+
+const router = express.Router();
+router.use(express.json());
+
+router
+  .route("/post/:post_id")
+  .get(likeValidationWith("post"), handleLikesReadWith("post"))
+  .post(requireLogin, likeValidationWith("post"), handleLikeCreateWith("post"))
+  .delete(
+    requireLogin,
+    likeValidationWith("post"),
+    handleLikeDeleteWith("post")
+  );
+
+router
+  .route("/comment/:comment_id")
+  .get(likeValidationWith("comment"), handleLikesReadWith("comment"))
+  .post(
+    requireLogin,
+    likeValidationWith("comment"),
+    handleLikeCreateWith("comment")
+  )
+  .delete(
+    requireLogin,
+    likeValidationWith("comment"),
+    handleLikeDeleteWith("comment")
+  );
+
+export default router;
