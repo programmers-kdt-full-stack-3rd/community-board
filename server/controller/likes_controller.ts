2f52d3ac59b91c0eff0145c496ef3817a8bb69f8
yejunian <leeye51456@gmail.com>
2024-07-13 03:32:36 +0900

    feat: likes controller 구현

---
+++ server/controller/likes_controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+import { createLike, deleteLike, readLikes } from "../db/context/likes_context";
+import { TLikeTarget } from "../db/model/likes";
+
+export const handleLikesReadWith =
+  <T extends TLikeTarget>(targetType: T) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const targetId = parseInt(req.params[`${targetType}_id`], 10);
+
+      const likes = await readLikes(targetType, targetId, req.userId);
+
+      res.status(200).json({
+        [`${targetType}_likes`]: likes,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+export const handleLikeCreateWith =
+  <T extends TLikeTarget>(targetType: T) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const targetId = parseInt(req.params[`${targetType}_id`], 10);
+
+      await createLike(targetType, targetId, req.userId);
+
+      res.status(201).end();
+    } catch (err) {
+      next(err);
+    }
+  };
+
+export const handleLikeDeleteWith =
+  <T extends TLikeTarget>(targetType: T) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const targetId = parseInt(req.params[`${targetType}_id`], 10);
+
+      await deleteLike(targetType, targetId, req.userId);
+
+      res.status(200).end();
+    } catch (err) {
+      next(err);
+    }
+  };
