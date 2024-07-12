12e18ebefa3bc29537769fc0d5649a3478f8b495
Siltarae <sjkwang1@gmail.com>
2024-07-12 16:45:57 +0900

    refator:입력 값에 대한 유효성 검사, API 엔드포인트에 대한 유효성 검사  분리

--- server/route/users_router.ts
+++ server/route/users_router.ts
@@ -7,84 +7,13 @@ import {
   handleCheckPassword,
   handleDeleteUser,
 } from "../controller/users_controller";
-import { body } from "express-validator";
-import { validate } from "../middleware/validate";
 import { requireLogin, requirePassword } from "../middleware/auth";
-
-// Request body 유효성 검사
-const joinValidation = [
-  body("email")
-    .notEmpty()
-    .withMessage("이메일을 입력해주십시오.")
-    .bail()
-    .isEmail()
-    .withMessage("이메일 형식이 아닙니다."),
-  body("password")
-    .notEmpty()
-    .withMessage("비밀번호를 입력해주십시오.")
-    .bail()
-    .isStrongPassword({
-      minLength: 10,
-      minLowercase: 1,
-      minUppercase: 1,
-      minNumbers: 1,
-      minSymbols: 0,
-    })
-    .withMessage("올바른 형태의 비밀번호가 아닙니다."),
-  body("nickname").notEmpty().withMessage("닉네임을 입력해주십시오."),
-  validate,
-];
-
-const loginValidation = [
-  body("email")
-    .notEmpty()
-    .withMessage("이메일을 입력해주십시오.")
-    .bail()
-    .isEmail()
-    .withMessage("이메일 또는 비밀번호가 틀렸습니다."),
-  body("password")
-    .notEmpty()
-    .withMessage("비밀번호를 입력해주십시오.")
-    .isStrongPassword({
-      minLength: 10,
-      minLowercase: 1,
-      minUppercase: 1,
-      minNumbers: 1,
-      minSymbols: 0,
-    })
-    .withMessage("이메일 또는 비밀번호가 틀렸습니다."),
-  validate,
-];
-
-const updateUserInfoValidation = [
-  body("nickname").notEmpty().withMessage("닉네임을 입력해주십시오."),
-  body("password")
-    .notEmpty()
-    .withMessage("비밀번호를 입력해주십시오.")
-    .isStrongPassword({
-      minLength: 10,
-      minLowercase: 1,
-      minUppercase: 1,
-      minNumbers: 1,
-      minSymbols: 0,
-    })
-    .withMessage("올바른 형태의 비밀번호가 아닙니다."),
-  validate,
-];
-
-const checkPasswordValidation = [
-  body("password")
-    .notEmpty()
-    .withMessage("비밀번호를 입력해주십시오.")
-    .isStrongPassword({
-      minLength: 10,
-      minLowercase: 1,
-      minUppercase: 1,
-      minNumbers: 1,
-      minSymbols: 0,
-    })
-    .withMessage("비밀번호가 틀렸습니다."),
-];
+import {
+  checkPasswordValidation,
+  joinValidation,
+  loginValidation,
+  updateUserInfoValidation,
+} from "../utils/validations/users/user";
 
 const router = express.Router();
 router.use(express.json());
---
+++ server/utils/validations/users/user.ts
@@ -0,0 +1,86 @@
+import { body } from "express-validator";
+import { validate } from "../../../middleware/validate";
+import { ERROR_MESSAGES, PASSWORD_POLICY } from "./constants";
+
+type ErrorMessageType = (typeof ERROR_MESSAGES)[keyof typeof ERROR_MESSAGES];
+
+/*
+  Validators
+  - 각 입력 값에 대한 유효성 검사
+*/
+
+/**
+ * 이메일 유효성 검사
+ * @param invalidMessage 유효하지 않은 이메일 형식에 대한 오류 메시지
+ */
+const emailValidator = (
+  invalidMessage: ErrorMessageType = ERROR_MESSAGES.INVALID_EMAIL
+) =>
+  body("email")
+    .notEmpty()
+    .withMessage(ERROR_MESSAGES.EMAIL_REQUIRED)
+    .bail()
+    .isEmail()
+    .withMessage(invalidMessage);
+
+/**
+ * 비밀번호 유효성 검사
+ * @param invalidMessage 유효하지 않은 비밀번호 형식에 대한 오류 메시지
+ */
+const passwordValidator = (
+  invalidMessage: ErrorMessageType = ERROR_MESSAGES.INVALID_PASSWORD
+) =>
+  body("password")
+    .notEmpty()
+    .withMessage(ERROR_MESSAGES.PASSWORD_REQUIRED)
+    .bail()
+    .isStrongPassword(PASSWORD_POLICY)
+    .withMessage(invalidMessage);
+
+/**
+ * 닉네임 유효성 검사
+ */
+const nicknameValidator = body("nickname")
+  .notEmpty()
+  .withMessage(ERROR_MESSAGES.NICKNAME_REQUIRED);
+
+/*
+  Validation
+  - 각 API 엔드포인트에 대한 유효성 검사
+*/
+
+/**
+ * 회원가입 API 유효성 검사
+ */
+export const joinValidation = [
+  emailValidator(),
+  passwordValidator(),
+  nicknameValidator,
+  validate,
+];
+
+/**
+ * 로그인 API 유효성 검사
+ */
+export const loginValidation = [
+  emailValidator(ERROR_MESSAGES.LOGIN_FAILED),
+  passwordValidator(ERROR_MESSAGES.LOGIN_FAILED),
+  validate,
+];
+
+/**
+ * 회원 정보 수정 API 유효성 검사
+ */
+export const updateUserInfoValidation = [
+  nicknameValidator,
+  passwordValidator(),
+  validate,
+];
+
+/**
+ * 비밀번호 확인 API 유효성 검사
+ */
+export const checkPasswordValidation = [
+  passwordValidator(ERROR_MESSAGES.PASSWORD_CHECK_FAILED),
+  validate,
+];
