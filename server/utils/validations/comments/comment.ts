c40d06f8f86af438c26f9cdc6bfe938827c61b6c
yejunian <leeye51456@gmail.com>
2024-07-12 22:57:21 +0900

    refactor: 댓글 API 유효성 검사 에러 메시지 추출

--- server/utils/validations/comments/comment.ts
+++ server/utils/validations/comments/comment.ts
@@ -1,14 +1,15 @@
 import { body, check, param, query } from "express-validator";
 import { validate } from "../../../middleware/validate";
+import { ERROR_MESSAGES } from "./constants";
 
 /** Request body의 댓글 본문 유효성 검사 */
 const bodyContentValidator = () =>
   body("content")
     .notEmpty()
-    .withMessage("본문을 입력해 주십시오.")
+    .withMessage(ERROR_MESSAGES.CONTENT_REQUIRED)
     .bail()
     .isString()
-    .withMessage("본문이 문자열이 아닙니다.");
+    .withMessage(ERROR_MESSAGES.INVALID_CONTENT);
 
 /**
  * 게시글 ID 유효성 검사
@@ -21,10 +22,10 @@ const postIdValidator = (
 ) =>
   checkFunction(name)
     .notEmpty()
-    .withMessage("게시글 ID를 입력해 주십시오.")
+    .withMessage(ERROR_MESSAGES.POST_ID_REQUIRED)
     .bail()
     .isInt({ min: 1 })
-    .withMessage("게시글 ID가 양의 정수가 아닙니다.");
+    .withMessage(ERROR_MESSAGES.INVALID_POST_ID);
 
 /**
  * 댓글 ID 유효성 검사
@@ -37,10 +38,10 @@ const commentIdValidator = (
 ) =>
   checkFunction(name)
     .notEmpty()
-    .withMessage("댓글 ID를 입력해 주십시오.")
+    .withMessage(ERROR_MESSAGES.COMMENT_ID_REQUIRED)
     .bail()
     .isInt({ min: 1 })
-    .withMessage("댓글 ID가 양의 정수가 아닙니다.");
+    .withMessage(ERROR_MESSAGES.INVALID_COMMENT_ID);
 
 /** 댓글 목록 조회 API 유효성 검사 */
 export const getCommentsValidation = [postIdValidator(query), validate];
