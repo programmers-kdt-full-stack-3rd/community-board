a4fa99721bab38a2640178b4636b102522f289e7
Siltarae <sjkwang1@gmail.com>
2024-07-14 23:26:00 +0900

    feat: 로그인화면으로 가는 라우트 추가,  유저화면에서 로그인화면으로 가는 버튼 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import TestMain from './component/TestMain/TestMain'
-import User from './component/User/User'
-import Posts from './component/Posts/Posts'
-import Tests from './component/TestMain/Tests'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import TestMain from "./component/TestMain/TestMain";
+import User from "./page/User/User";
+import Posts from "./component/Posts/Posts";
+import Tests from "./component/TestMain/Tests";
+import Login from "./page/User/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TestMain />}/>
-        <Route path="/user" element={<User/>}/>
-        <Route path="/test" element={<Tests/>}/>
-        <Route path="/posts" element={<Posts/>}/>
+        <Route path="/" element={<TestMain />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/test" element={<Tests />} />
+        <Route path="/posts" element={<Posts />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/page/User/User.tsx
+++ client/src/page/User/User.tsx
@@ -1,5 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const User = () => {
-  return <div>User</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button onClick={() => navigate("/login")}>로그인 화면</button>
+    </div>
+  );
 };
 
 export default User;
