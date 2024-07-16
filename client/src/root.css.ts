ffbb6ad3613ef698f03b6ef3cf61a5985d17b64e
Siltarae <sjkwang1@gmail.com>
2024-07-16 14:02:27 +0900

    Refactor: App 레이아웃 수정 및 전역 스타일 조정

---
+++ client/src/root.css.ts
@@ -0,0 +1,10 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("#root", {
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  height: "100%",
+  margin: "0 auto",
+  textAlign: "center",
+});
