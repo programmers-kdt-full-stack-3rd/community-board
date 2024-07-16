df172c282e6a75dc40003479a6ec28e3b5ae1e00
Siltarae <sjkwang1@gmail.com>
2024-07-16 23:09:53 +0900

    feat: 비밀번호 확인 API 추가

--- client/src/api/users/crud.ts
+++ client/src/api/users/crud.ts
@@ -8,6 +8,14 @@ export const sendPostJoinRequest = async (body: object) => {
   return await httpRequest("user/Join", HttpMethod.POST, convertToBody(body));
 };
 
+export const sendPOSTCheckPasswordRequest = async (body: object) => {
+  return await httpRequest(
+    "user/check-password",
+    HttpMethod.POST,
+    convertToBody(body)
+  );
+};
+
 export const sendPostLogoutRequest = async () => {
   return await httpRequest("user/logout", HttpMethod.POST);
 };
