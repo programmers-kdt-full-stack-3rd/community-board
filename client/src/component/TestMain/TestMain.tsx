bf7f3cc036c643ba65e666ec0d43b2e335cf9097
Siltarae <sjkwang1@gmail.com>
2024-07-16 14:01:47 +0900

    refactor: App.css제거를위해 style 분리

--- client/src/component/TestMain/TestMain.tsx
+++ client/src/component/TestMain/TestMain.tsx
@@ -1,20 +1,19 @@
 import { useNavigate } from "react-router-dom";
+import { card, main, readTheDocs } from "./TestMain.css";
 
 const TestMain = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="main">
-            <div className="card">
-                <button onClick={()=>navigate("/user")}>회원 기능</button>
-                <button onClick={()=>navigate("/test")}>테스트 보기</button>
-                <button onClick={()=>navigate("/posts")}>게시글 기능</button>
-            </div>
-            <p className="read-the-docs">
-                상단의 개발 할 컴포넌트 클릭
-            </p>
-        </div>
-    );
-}
+  return (
+    <div className={main}>
+      <div className={card}>
+        <button onClick={() => navigate("/user")}>회원 기능</button>
+        <button onClick={() => navigate("/test")}>테스트 보기</button>
+        <button onClick={() => navigate("/posts")}>게시글 기능</button>
+      </div>
+      <p className={readTheDocs}>상단의 개발 할 컴포넌트 클릭</p>
+    </div>
+  );
+};
 
 export default TestMain;
