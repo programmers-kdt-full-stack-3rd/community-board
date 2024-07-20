e8255723c72bb1df8440984cb7015c0475a2e8aa
Siltarae <sjkwang1@gmail.com>
2024-07-20 22:01:29 +0900

    feat: 비밀번호입력시 복사/붙여넣기 방지 추가

--- client/src/component/User/PasswordForm.tsx
+++ client/src/component/User/PasswordForm.tsx
@@ -16,6 +16,9 @@ const PasswordForm: FC<IPasswordFormProps> = ({
   onChange,
   isValid = true,
 }) => {
+  const preventCopyPaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+    e.preventDefault();
+  };
   return (
     <InputField
       labelText={labelText}
@@ -25,6 +28,9 @@ const PasswordForm: FC<IPasswordFormProps> = ({
       onChange={onChange}
       placeholder="비밀번호를 입력하세요."
       isValid={isValid}
+      onCopy={preventCopyPaste}
+      onPaste={preventCopyPaste}
+      onCut={preventCopyPaste}
     />
   );
 };
