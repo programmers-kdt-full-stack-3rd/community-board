0e2debdeaf2bcbd78688c6aad7a4cba7be192e76
HungKungE <jonsun59@gmail.com>
2024-07-21 04:30:33 +0900

    feat : commentItem 에러 처리

--- client/src/component/Comments/CommentItem/CommentItem.tsx
+++ client/src/component/Comments/CommentItem/CommentItem.tsx
@@ -18,6 +18,9 @@ import {
   commentFooter,
   commentEditButtons,
 } from "./CommentItem.css";
+import { ApiCall } from "../../../api/api";
+import { useErrorModal } from "../../../state/errorModalStore";
+import { ClientError } from "../../../api/errors";
 
 interface ICommentItemProps {
   comment: IComment;
@@ -26,6 +29,7 @@ interface ICommentItemProps {
 
 const CommentItem = ({ comment, onUpdate }: ICommentItemProps) => {
   const [isEditMode, setIsEditMode] = useState(false);
+  const errorModal = useErrorModal();
 
   const contentNodes = useMemo(
     () =>
@@ -49,28 +53,24 @@ const CommentItem = ({ comment, onUpdate }: ICommentItemProps) => {
     const trimmedContent = content.trim();
 
     if (trimmedContent === comment.content) {
-      alert("댓글 내용이 이전과 동일합니다.");
+      errorModal.setErrorMessage("error:댓글 내용이 이전과 동일합니다.");
+      errorModal.open();
       return false;
     }
 
-    try {
-      const response = await sendPatchCommentRequest({
-        content,
-        id: comment.id,
-      });
+    const res = await ApiCall(
+        () => sendPatchCommentRequest({content,id: comment.id}),
+        (err) => {
+          errorModal.setErrorMessage(err.message);
+          errorModal.open();
+        }
+    );
 
-      if (response?.status >= 400) {
-        console.error(response);
-        alert("댓글 수정에 실패했습니다.");
-        return false;
-      }
-
-      alert("댓글을 수정했습니다.");
-    } catch (error) {
-      console.error(error);
-      alert("댓글 수정에 실패했습니다.");
+    if(res instanceof ClientError){
       return false;
     }
+
+    alert("댓글을 수정했습니다.");
 
     if (onUpdate) {
       await onUpdate();
@@ -88,19 +88,15 @@ const CommentItem = ({ comment, onUpdate }: ICommentItemProps) => {
       return;
     }
 
-    try {
-      const response = await sendDeleteCommentRequest(comment.id);
-
-      if (response?.status >= 400) {
-        console.error(response);
-        alert("댓글 삭제에 실패했습니다.");
-        return false;
+    const res = await ApiCall(
+      () => sendDeleteCommentRequest(comment.id),
+      (err) => {
+        errorModal.setErrorMessage(err.message);
+        errorModal.open();
       }
+    );
 
-      alert("댓글을 삭제했습니다.");
-    } catch (error) {
-      console.error(error);
-      alert("댓글 삭제에 실패했습니다.");
+    if (res instanceof ClientError){
       return false;
     }
 
