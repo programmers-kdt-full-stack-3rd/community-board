f69a67022f6b286b46dc69bafed3abc39d265b36
HungKungE <jonsun59@gmail.com>
2024-08-13 15:47:47 +0900

    ✨ [feat] : 채팅방 컴포넌트 클릭 -> 입장 버튼 (비밀방이면 input도) 렌더링

--- client/src/component/Chats/ChatRooms/Rooms/Room.tsx
+++ client/src/component/Chats/ChatRooms/Rooms/Room.tsx
@@ -2,6 +2,7 @@ import {
 	chatContainer,
 	lockIcon,
 	numContainer,
+	passwordInput,
 	roomContainer,
 	roomHeaderContainer,
 	roomWrapper,
@@ -10,6 +11,7 @@ import {
 import { IRoomHeader } from "./Rooms";
 import { CiLock } from "react-icons/ci";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 interface Props {
 	room: IRoomHeader;
@@ -19,12 +21,28 @@ interface Props {
 
 const Room: React.FC<Props> = ({ room, isMine, index }) => {
 	const navigate = useNavigate();
+	const [open, setOpen] = useState(false);
+	const [password, setPassword] = useState("");
+
+	const enterRoom = () => {
+		// TODO: 해당 room으로 이동 -> aside에서 가능하도록 변경
+		if (!room.is_private) {
+			navigate(`/room/${room.roomId}`);
+			return;
+		}
+
+		// 서버로 비밀번호 확인 요청
+		// 비밀번호 일치하면 방 입장
+		navigate(`/room/${room.roomId}`);
+	};
 
 	const onRoomClick = () => {
-		// TODO: 비밀방 입장시 비밀번호 입력 모달 생성
+		if (isMine) {
+			enterRoom();
+			return;
+		}
 
-		// TODO: 해당 room으로 이동 -> aside에서 가능하도록 변경
-		navigate(`/room/${room.roomId}`);
+		setOpen(true);
 	};
 
 	return (
@@ -32,30 +50,55 @@ const Room: React.FC<Props> = ({ room, isMine, index }) => {
 			className={roomWrapper}
 			key={index}
 		>
-			<div
-				className={roomContainer}
-				onClick={onRoomClick}
-			>
-				<div className={roomHeaderContainer}>
-					<div className={titleContainer}>
+			{open ? (
+				<div className={roomContainer}>
+					<div className={roomHeaderContainer}>
+						<div className={titleContainer}>
+							{room.is_private ? (
+								<CiLock className={lockIcon} />
+							) : (
+								" "
+							)}
+							채팅방 이름: {room.title}
+						</div>
 						{room.is_private ? (
-							<CiLock className={lockIcon} />
-						) : (
-							" "
-						)}
-						채팅방 이름: {room.title}
-					</div>
-					<div className={numContainer}>
-						{isMine ? <span>접속 인원: {room.curNum}</span> : null}
-						<span>참여 인원: {room.participantNum}</span>
+							<input
+								className={passwordInput}
+								value={password}
+								onChange={e => setPassword(e.target.value)}
+							/>
+						) : null}
+						<button onClick={enterRoom}>입장하기</button>
 					</div>
 				</div>
-				{isMine ? (
-					<div className={chatContainer}>
-						<span>최근 메시지: {room.lastMessage}</span>
+			) : (
+				<div
+					className={roomContainer}
+					onClick={onRoomClick}
+				>
+					<div className={roomHeaderContainer}>
+						<div className={titleContainer}>
+							{room.is_private ? (
+								<CiLock className={lockIcon} />
+							) : (
+								" "
+							)}
+							채팅방 이름: {room.title}
+						</div>
+						<div className={numContainer}>
+							{isMine ? (
+								<span>접속 인원: {room.curNum}</span>
+							) : null}
+							<span>참여 인원: {room.participantNum}</span>
+						</div>
 					</div>
-				) : null}
-			</div>
+					{isMine ? (
+						<div className={chatContainer}>
+							<span>최근 메시지: {room.lastMessage}</span>
+						</div>
+					) : null}
+				</div>
+			)}
 		</div>
 	);
 };
