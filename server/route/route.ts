c988aa86bfd19b5e20062306167a855284b16b11
Siltarae <sjkwang1@gmail.com>
2024-08-14 23:18:13 +0900

    ✨ [feat] : admin 라우팅 및 사용자 목록 조회 API 추가

--- server/route/route.ts
+++ server/route/route.ts
@@ -3,6 +3,7 @@ import postRouter from "./posts_router";
 import userRouter from "./users_router";
 import commentRouter from "./comments_router";
 import likeRouter from "./likes_router";
+import adminRouter from "./admin_router";
 
 const router = express.Router();
 router.use(express.json());
@@ -12,5 +13,6 @@ router.use("/post", postRouter);
 router.use("/comment", commentRouter);
 router.use("/user", userRouter);
 router.use("/like", likeRouter);
+router.use("/admin", adminRouter);
 
 export default router;
