d62ff783029ac89301b8bd404f902b27f3088b9a
HungKungE <jonsun59@gmail.com>
2024-08-18 19:17:47 +0900

    ✨ [feat] : [BE] create room api 구현

--- server/db/sql/chats.sql
+++ server/db/sql/chats.sql
@@ -2,20 +2,22 @@ CREATE TABLE IF NOT EXISTS rooms (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     is_private BOOLEAN DEFAULT FALSE,
+    password VARCHAR(50),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP ON UPDATE NOW(),
+    updated_at TIMESTAMP ON UPDATE NOW()
 );
 
-CREATE TABLE IF NOT EXISTS participants (
-    id INT NOT NULL PRIMARY KEY,
-    room_id INT NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS members (
+    id INT NOT NULL,
+    room_id INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     is_host Boolean DEFAULT FALSE,
     is_deleted Boolean DEFAULT FALSE,
     last_message_id INT,
+    PRIMARY KEY (id, room_id),
     FOREIGN KEY (room_id) REFERENCES rooms(id),
     FOREIGN KEY (id) REFERENCES users(id),
-    FOREIGN KEY (last_message_id) REFERENCES messages(id),
+    FOREIGN KEY (last_message_id) REFERENCES messages(id)
 );
 
 /* user_id, room_id에 대한 index 고려하기 */
@@ -26,5 +28,5 @@ CREATE TABLE IF NOT EXISTS messages (
     content TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (room_id) REFERENCES rooms(id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
--- server/route/chats_router.ts
+++ server/route/chats_router.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { handleRoomCreate } from "../controller/chats_controller";
 
 const router = express.Router();
 router.use(express.json());
 
-router.get("/", () => {});
+router.post("/room", handleRoomCreate);
 
 export default router;
