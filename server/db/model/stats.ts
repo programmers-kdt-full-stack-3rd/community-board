a6db1fdcb79ce5e5c0fbd167393d25b123ef9384
Siltarae <sjkwang1@gmail.com>
2024-08-19 19:07:20 +0900

    ✨ [feat] : stats_context에 전체/기간별 통계 조회 기능 추가

---
+++ server/db/model/stats.ts
@@ -0,0 +1,38 @@
+import { RowDataPacket } from "mysql2/promise";
+
+export interface IStatResult extends RowDataPacket {
+	count: number;
+}
+
+export interface IStatResultInterval extends IStatResult {
+	date: string;
+}
+
+export interface IPostResult extends IStatResult {
+	views: number;
+}
+
+export interface IPostResultInterval extends IPostResult {
+	date: string;
+}
+
+export interface IStats {
+	posts: number;
+	comments: number;
+	views: number;
+	users: number;
+}
+
+export interface IStatsInterval {
+	posts: IPostResultInterval[];
+	comments: IStatResultInterval[];
+	users: IStatResultInterval[];
+}
+
+export interface IStatsIntervalInput {
+	startDate?: string;
+	endDate?: string;
+	interval: TInterval;
+}
+
+export type TInterval = "daily" | "monthly" | "yearly";
