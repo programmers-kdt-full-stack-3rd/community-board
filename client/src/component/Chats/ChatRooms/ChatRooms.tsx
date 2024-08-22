566315abeff41d4355ef53ce1431482363e99b40
장 재 훈 <peng2412@naver.com>
2024-08-22 21:40:02 +0900

    ✨ [feat] : aside 열 때, get_my_rooms 이벤트

--- client/src/component/Chats/ChatRooms/ChatRooms.tsx
+++ client/src/component/Chats/ChatRooms/ChatRooms.tsx
@@ -1,9 +1,11 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { container } from "./ChatRooms.css";
-import { IRoomHeader } from "shared";
+import { IGetMyRoomRequestEvent, IRoomHeader } from "shared";
 import CreateRoomModal from "./Modal/CreateRoomModal";
 import MyChatRooms from "./MyChatRooms";
 import SearchedChatRooms from "./SearchedChatRooms";
+import { Socket } from "socket.io-client";
+import { useUserStore } from "../../../state/store";
 
 export interface RoomsInfo {
 	totalRoomCount: number;
@@ -12,8 +14,21 @@ export interface RoomsInfo {
 	};
 }
 
-const ChatRooms: FC = () => {
+interface ChatRoomsProps {
+	socket: Socket | null;
+}
+
+const ChatRooms: FC<ChatRoomsProps> = ({ socket }) => {
 	const [isOpen, setIsOpen] = useState(false);
+	const isAsideOpen = true; // TODO : Aside UI 만들때 State 관리
+	const nickname = useUserStore(state => state.nickname);
+
+	useEffect(() => {
+		if (isAsideOpen) {
+			const data: IGetMyRoomRequestEvent = { nickname };
+			socket?.emit("get_my_rooms", data);
+		}
+	}, [isAsideOpen, socket]);
 
 	return (
 		<div className={container}>
