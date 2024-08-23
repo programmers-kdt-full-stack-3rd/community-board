e1b89b156813bd05b89ecca11aaca2b6eac691ed
una3325 <a33256448@gmail.com>
2024-08-24 08:26:07 +0900

    ✨ [feat] : 로딩 화면 구현

---
+++ client/src/component/common/Loader/Loader.css.ts
@@ -0,0 +1,26 @@
+import { style, keyframes } from "@vanilla-extract/css";
+
+const spin = keyframes({
+	from: { transform: "rotate(0deg)" },
+	to: { transform: "rotate(360deg)" },
+});
+
+export const iconStyle = style({
+	fontSize: "50px",
+	animation: `${spin} 1s linear infinite`,
+	color: "#bdbdbd",
+});
+
+export const containerStyle = style({
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "center",
+	alignItems: "center",
+	height: "100vh",
+});
+
+export const textStyle = style({
+	marginTop: "20px",
+	fontSize: "16px",
+	color: "#fff",
+});
