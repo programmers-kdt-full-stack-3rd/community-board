5fd59cb8c6b728b9752a176d4eeefbc5abb9c5e2
inhwa <dlsghk0711@naver.com>
2024-08-25 04:32:13 +0900

    ✨ [feat] : 통계 API 요청

---
+++ client/src/component/Admin/Stats/Stats.tsx
@@ -0,0 +1,17 @@
+import { TInterval } from "shared";
+import { HttpMethod, httpRequest } from "../../../api/api";
+
+export const fetchTotalStats = async (startDate: string, endDate: string, interval: TInterval) => {
+    const URL = `admin/stat?startDate=${startDate}&endDate=${endDate}&interval=${interval}`;
+    console.log(URL);
+    try {
+        const response = await httpRequest(URL, HttpMethod.GET);
+        if (response.status >= 400) {
+            const errorText = response.message || 'API 응답 오류';
+            throw new Error(errorText);
+        }
+        return response;
+    } catch (err) {
+        throw err;
+    }
+};
