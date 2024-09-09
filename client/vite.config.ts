5f1164643694e6e3d5982083c19fa8edf065047b
yejunian <leeye51456@gmail.com>
2024-09-10 02:12:23 +0900

    🚀 [chore] : watch 스크립트 실행 시 chunk size limit 완화

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,9 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), vanillaExtractPlugin()],
+
+	build: {
+		chunkSizeWarningLimit:
+			process.env.NODE_ENV === "development" ? 2000 : undefined,
+	},
 });
