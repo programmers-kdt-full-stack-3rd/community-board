b76dbea48521d395c500aa913abfcb4e58415939
Siltarae <sjkwang1@gmail.com>
2024-09-09 19:02:17 +0900

    ✨ [feat] : validation pipe 추가

--- nestjs-migration/src/main.ts
+++ nestjs-migration/src/main.ts
@@ -1,11 +1,22 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import * as cookieParser from "cookie-parser";
+import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import * as cookieParser from "cookie-parser";
+import { AppModule } from "./app.module";
+import { ServerError } from "./common/exceptions/server-error.exception";
 import { GlobalExceptionFilter } from "./common/filters/global-exception.filter";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			disableErrorMessages: true,
+			exceptionFactory: errors => {
+				const errorMessage = Object.values(errors[0].constraints)[0];
+				throw ServerError.badRequest(errorMessage);
+			},
+		})
+	);
 	app.useGlobalFilters(new GlobalExceptionFilter());
 	app.use(cookieParser());
 	app.setGlobalPrefix("api");
