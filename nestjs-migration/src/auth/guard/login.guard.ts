4d4eddc1426095cf7b9e3072b8a7a6d7bedda05c
Siltarae <sjkwang1@gmail.com>
2024-09-12 01:14:08 +0900

    ✨ [feat] : 로그인 가드 추가

---
+++ nestjs-migration/src/auth/guard/login.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { ServerError } from "../../common/exceptions/server-error.exception";
+
+@Injectable()
+export class LoginGuard implements CanActivate {
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request = context.switchToHttp().getRequest();
+
+		if (!request.user || !request.user.userId) {
+			throw ServerError.unauthorized("로그인이 필요합니다.");
+		}
+
+		return true;
+	}
+}
