e267f3792518bec69fe7c4944232455670b58d1c
HungKungE <jonsun59@gmail.com>
2024-09-12 20:10:36 +0900

    ✨ [feat] : nginx - socket, kafka 연결

---
+++ docker-command.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $1 == "ka" ]; then
+    docker-compose -f docker-compose-kafka.yaml -p kafka up -d
+elif [ $1 == "co" ]; then
+    docker-compose -f docker-compose-server.yaml -p community up -d
+elif [ $1 == "del-ka" ]; then
+    docker-compose -f docker-compose-kafka.yaml -p kafka down
+elif [ $1 == "del-co" ]; then
+    docker-compose -f docker-compose-server.yaml -p community down
+elif [ $1 == "del-cache" ]; then
+    docker system prune --all --volumes
+elif [ $1 == "push" ]; then
+  docker push hungkunge/community_board;
+else
+    echo "wrong command!";
+fi
