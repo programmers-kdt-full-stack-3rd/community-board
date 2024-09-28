50367f86515eb4a07876b88038f245e2be37a208
Siltarae <sjkwang1@gmail.com>
2024-09-28 14:36:52 +0900

    🐛 [fix] : dto에 불필요한 내용 제거

--- nestjs-migration/src/admin/dto/get-stats.dto.ts
+++ nestjs-migration/src/admin/dto/get-stats.dto.ts
@@ -1,18 +1,13 @@
-import { Type } from "class-transformer";
-import { IsDateString, IsOptional, IsString, Matches } from "class-validator";
+import { IsOptional, IsString, Matches } from "class-validator";
 import { TInterval } from "shared";
 
 export class GetStatsQueryDto {
 	@IsOptional()
-	@IsDateString({}, { message: "startDate가 잘못되었습니다." })
 	@Matches(/^\d{4}-\d{2}-\d{2}$/, { message: "startDate가 잘못되었습니다." })
-	@Type(() => String)
 	startDate?: string;
 
 	@IsOptional()
-	@IsDateString({}, { message: "endDate가 잘못되었습니다." })
 	@Matches(/^\d{4}-\d{2}-\d{2}$/, { message: "endDate가 잘못되었습니다." })
-	@Type(() => String)
 	endDate?: string;
 
 	@IsOptional()
