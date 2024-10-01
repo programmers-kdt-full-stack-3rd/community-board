05c8c17edc6a6e3936fb864e03a5981413190505
HungKungE <jonsun59@gmail.com>
2024-10-01 17:42:54 +0900

    💄 [style] : 파일 이름 오타 수정

--- server/route/image_router.ts
+++ server/route/image_router.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { requireLogin } from "../middleware/auth";
 import { upload } from "../utils/upload-image";
-import { handleUploadImage } from "../controller/image_comtroller";
+import { handleUploadImage } from "../controller/ image_controller";
 
 const router = express.Router();
 router.use(express.json());
