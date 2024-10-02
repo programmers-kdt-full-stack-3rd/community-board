1ccb3f2a4b3ae39e8eaba400a3a89c20dd0d54d8
HungKungE <jonsun59@gmail.com>
2024-10-02 19:50:33 +0900

    🐛 [fix] : S3 버킷 업로드 api 미들웨어 실행되지 않던 오류 수정

--- server/utils/upload-image.ts
+++ server/utils/upload-image.ts
@@ -17,31 +17,42 @@ export const upload = async (
 		if (
 			!process.env.REGION ||
 			!process.env.ACCESS_KEY_ID ||
-			!process.env.SECRET_ACCESS_KEY
+			!process.env.SECRET_ACCESS_KEY ||
+			!process.env.BUCKET_NAME
 		) {
 			throw new ServerError(500, "aws config is undefined");
 		}
 
 		const s3 = new S3Client({
-			region: process.env.REGION || "",
+			region: process.env.REGION,
 			credentials: {
-				accessKeyId: process.env.ACCESS_KEY_ID || "",
-				secretAccessKey: process.env.SECRET_ACCESS_KEY || "",
+				accessKeyId: process.env.ACCESS_KEY_ID,
+				secretAccessKey: process.env.SECRET_ACCESS_KEY,
 			},
 		});
 
-		return multer({
+		const uploadMiddleware = multer({
 			storage: multerS3({
 				s3,
-				bucket: "hungkung-burket", // S3 버킷 이름
+				bucket: process.env.BUCKET_NAME, // S3 버킷 이름
+				contentType: multerS3.AUTO_CONTENT_TYPE,
 				key: (req, file, cb) => {
 					// 파일명 설정
 					const fileName = `${Date.now()}_${file.originalname}`;
 					cb(null, fileName);
 				},
 			}),
-			// limits: { fileSize: 5 * 1024 * 1024 },
+			// limits: { fileSize: 5 * 1024 * 1024 }, // 파일 크기 제한 설정 가능
 		}).single("image");
+
+		// 생성한 `multer` 미들웨어를 즉시 실행
+		uploadMiddleware(req, res, (err: any) => {
+			if (err) {
+				return next(err); // 에러가 있으면 next로 넘김
+			}
+			// 에러 없이 완료되면 다음 미들웨어 호출
+			next();
+		});
 	} catch (err) {
 		next(err);
 	}
