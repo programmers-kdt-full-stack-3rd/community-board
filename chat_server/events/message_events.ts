cfb1359e03d256a8e4d72765a0dbe938c894e14c
hojun3377 <hojun3377@gmail.com>
2024-10-08 11:25:28 +0900

    ✨ [feat] : 메시지 캐싱
    Redis가 켜진 후 특정 채팅방 첫 입장 시에만 해당 채팅방 모든 메시지 HTTP 요청
    이후 Redis에 모든 메시지가 저장되어 채팅 메시지들 캐싱
    
    Github issues [#162]

--- chat_server/events/message_events.ts
+++ chat_server/events/message_events.ts
@@ -2,22 +2,28 @@ import { IKafkaMessageDTO, IMessage, ISocketMessageDTO } from "shared";
 import { Socket } from "socket.io";
 
 import { processMessage } from "../services/chat_service";
+import { setMessages } from "../services/redis_service";
 
 // 메세지 이벤트
 export const handleMessageEvents = (socket: Socket) => {
 	socket.on("send_message", async (message: ISocketMessageDTO, callback) => {
-		const createdAt = new Date(message.createdAt);
-
-		const msg: IKafkaMessageDTO = {
-			...message,
-			createdAt,
-		};
-
 		try {
+			const roomId = message.roomId;
+			const createdAt = new Date(message.createdAt);
+			const msg: IKafkaMessageDTO = {
+				...message,
+				createdAt,
+			};
+			const redisMessage: IMessage = {
+				...message,
+				createdAt,
+				isSystem: false,
+			};
+
 			await processMessage(msg);
 			callback(true);
 
-			// TODO : 캐시 저장
+			await setMessages(roomId, [redisMessage]);
 
 			const receiveMsg: IMessage = {
 				...message,
