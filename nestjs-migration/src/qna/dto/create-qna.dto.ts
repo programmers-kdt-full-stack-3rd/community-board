31c829220be1b69867e89df7cb35baaee4181640
hojun3377 <hojun3377@gmail.com>
2024-10-17 09:32:21 +0900

    ✨ [feat]: 댓글 채택 API DTO 정의
    - 채택 정보 조회(res, req) DTO
    - 채택 생성(res) DTO
    
    Github issues [#230]

---
+++ nestjs-migration/src/qna/dto/create-qna.dto.ts
@@ -0,0 +1,11 @@
+import { IsInt, Min } from "class-validator";
+
+export class AcceptQnACommentReq {
+	@IsInt({ message: "postId가 숫자가 아닙니다." })
+	@Min(1, { message: "postId가 1보다 커야합니다." })
+	postId: number;
+
+	@IsInt({ message: "commendId가 숫자가 아닙니다." })
+	@Min(1, { message: "commendId가 1보다 커야합니다." })
+	commentId: number;
+}
