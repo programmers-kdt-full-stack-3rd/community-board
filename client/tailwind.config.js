48e67e0c4da5ac60dccd39d2474d051aa12100e6
HungKungE <jonsun59@gmail.com>
2024-10-17 18:00:27 +0900

    ✨ [feat] : Dockerfile 수정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{js,ts,jsx,tsx,css}'],
-  theme: {
-    extend: {
-      colors: {
-        customGray: '#333333',
-        customDarkGray: '#242424',
-      },
-    },
-  },
-  plugins: [],
+	darkMode: "class",
+	content: ["./src/**/*.{js,ts,jsx,tsx,css}"],
+	theme: {
+		extend: {
+			colors: {
+				customGray: "#333333",
+				customDarkGray: "#242424",
+			},
+		},
+	},
+	plugins: [],
 };
