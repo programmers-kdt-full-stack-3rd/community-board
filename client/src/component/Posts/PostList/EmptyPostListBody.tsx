5323df65ea2abcfa820bec601919937c7438b1bd
yejunian <leeye51456@gmail.com>
2024-10-17 17:14:54 +0900

    🐛 [fix] : 게시글 목록에 게시글이 없을 때 여백 확보

--- client/src/component/Posts/PostList/EmptyPostListBody.tsx
+++ client/src/component/Posts/PostList/EmptyPostListBody.tsx
@@ -1,27 +1,29 @@
+import React from "react";
+
 interface IEmptyPostListBody {
-	className?: string;
 	isFetchFailed: boolean;
 	keyword?: string;
 }
 
-const EmptyPostListBody = ({
-	className,
+const EmptyPostListBody: React.FC<IEmptyPostListBody> = ({
 	isFetchFailed,
 	keyword,
-}: IEmptyPostListBody) => {
+}) => {
 	return (
-		<p className={className}>
-			{isFetchFailed ? (
-				"게시글을 불러오지 못했습니다."
-			) : keyword ? (
-				<>“{keyword}”에 대한 검색 결과가 없습니다.</>
-			) : (
-				<>
-					아직 게시글이 없습니다.
-					<br />첫 게시글을 작성해 보세요.
-				</>
-			)}
-		</p>
+		<div className="my-30 flex min-h-48 items-center justify-center text-center text-lg text-gray-600 dark:text-gray-500">
+			<p>
+				{isFetchFailed ? (
+					"게시글을 불러오지 못했습니다."
+				) : keyword ? (
+					<>“{keyword}”에 대한 검색 결과가 없습니다.</>
+				) : (
+					<>
+						아직 게시글이 없습니다.
+						<br />첫 게시글을 작성해 보세요.
+					</>
+				)}
+			</p>
+		</div>
 	);
 };
 
