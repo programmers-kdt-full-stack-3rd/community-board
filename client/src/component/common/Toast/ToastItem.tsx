6daac320e37788530a47f654f13c377a8e4c5614
yejunian <leeye51456@gmail.com>
2024-11-24 21:09:40 +0900

    ✨ [feat] : Toast 다크 모드 스타일 작성

--- client/src/component/common/Toast/ToastItem.tsx
+++ client/src/component/common/Toast/ToastItem.tsx
@@ -24,26 +24,22 @@ type TVariantProperties<T> = {
 
 const TOAST_DURATION_MS = 3000;
 
-const baseClassNames: IToastClassNames = {
-	box: "bg-customGray",
-	icon: "text-lg",
-};
 const variantClassNames: TVariantProperties<IToastClassNames> = {
 	error: {
-		box: "bg-red-900",
-		icon: "text-red-200",
+		box: "bg-red-200 dark:bg-red-900",
+		icon: "text-red-900 dark:text-red-200",
 	},
 	warning: {
-		box: "bg-yellow-900",
-		icon: "text-yellow-200",
+		box: "bg-yellow-200 dark:bg-yellow-900",
+		icon: "text-yellow-900 dark:text-yellow-200",
 	},
 	success: {
-		box: "bg-green-900",
-		icon: "text-green-200",
+		box: "bg-green-200 dark:bg-green-900",
+		icon: "text-green-900 dark:text-green-200",
 	},
 	info: {
 		box: "",
-		icon: "text-neutral-200",
+		icon: "text-neutral-800 dark:text-neutral-200",
 	},
 };
 
@@ -120,7 +116,6 @@ const ToastItem: React.FC<IProps> = ({ id }) => {
 		};
 	}, [item?.phase]);
 
-	// TODO: 다크모드 스타일 작성
 	return (
 		<div
 			className="fixed left-1/2 top-20 z-50 w-full max-w-96 -translate-x-1/2 translate-y-0 transition-transform duration-300"
@@ -132,8 +127,7 @@ const ToastItem: React.FC<IProps> = ({ id }) => {
 				ref={toastBoxRef}
 				onClick={handleDismiss}
 				className={twMerge(
-					"bg-customGray flex w-full cursor-pointer gap-2 rounded bg-opacity-85 py-3 pl-3 pr-4 text-sm text-white shadow-md backdrop-blur-xl transition duration-300",
-					baseClassNames.box,
+					"dark:bg-customGray flex w-full cursor-pointer gap-2 rounded border-l border-t border-white/5 bg-neutral-200 py-3 pl-3 pr-4 text-sm text-black shadow-md transition duration-300 dark:text-white",
 					variantClassNames[variant].box,
 					item?.phase === "in" && "opacity-0",
 					item?.phase === "out" && "-translate-y-6 opacity-0"
@@ -142,7 +136,7 @@ const ToastItem: React.FC<IProps> = ({ id }) => {
 				<div className="shrink-0 grow-0">
 					<Icon
 						className={twJoin(
-							baseClassNames.icon,
+							"text-lg",
 							variantClassNames[variant].icon
 						)}
 					/>
