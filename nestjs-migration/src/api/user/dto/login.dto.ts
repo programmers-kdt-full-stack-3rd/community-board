86e7dac775195aa742044ae5819fe6627157ca35
HungKungE <jonsun59@gmail.com>
2024-12-01 03:24:22 +0900

    ♻️ [refactor] : email, password 양식 에러 메세지 통일

--- nestjs-migration/src/api/user/dto/login.dto.ts
+++ nestjs-migration/src/api/user/dto/login.dto.ts
@@ -5,13 +5,13 @@ import {
 } from "../constant/user.constants";
 
 export class LoginDto {
-	@IsEmail({}, { message: VALIDATION_ERROR_MESSAGES.INVALID_EMAIL })
+	@IsEmail({}, { message: VALIDATION_ERROR_MESSAGES.LOGIN_FAILED })
 	@IsNotEmpty({ message: VALIDATION_ERROR_MESSAGES.EMAIL_REQUIRED })
 	email: string;
 
 	@IsNotEmpty({ message: VALIDATION_ERROR_MESSAGES.PASSWORD_REQUIRED })
 	@IsStrongPassword(PASSWORD_POLICY, {
-		message: VALIDATION_ERROR_MESSAGES.INVALID_PASSWORD,
+		message: VALIDATION_ERROR_MESSAGES.LOGIN_FAILED,
 	})
 	password: string;
 }
